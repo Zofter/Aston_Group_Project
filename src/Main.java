cea132fdf5bfe57322bb765695b5d800a66aad29
user <user@DESKTOP>
2025-09-09 13:38:31 +0400

    Set simple Strategy pattern

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,37 @@
-import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
         boolean workInMainCycle = true;
-        Scanner in = new Scanner(System.in);
-        while (workInMainCycle) {
-            System.out.println("Укажите длину кастомной коллекции");
-            int collectionSize = Integer.parseInt(in.next());
-            System.out.println("Для выбора заполнения коллекции случайными параметрами, введите \"random\"");
-            System.out.println("Для выбора заполнения коллекции собственными значениями, введите \"self\"");
-            System.out.println("Для выбора заполнения коллекции значениями из файла, введите \"file\"");
-            System.out.println("Для выхода из программы введите команду \"exit\"");
-            String typeFillingCollection = in.next();
-            if (in.next().equals("exit")) {
-                in.close();
-                workInMainCycle = false;
-            }
-            List<Person> personCollection = fillCustomCollection(typeFillingCollection, collectionSize);
-        }
-        System.out.println("Выход из главного цикла");
+        int collectionSize = 0;
+
+        Person person = PersonPull.createPerson(new ManualStrategy());
+        System.out.println(person);
+
+//        Scanner in = new Scanner(System.in);
+//        while (workInMainCycle) {
+//            System.out.println("Укажите длину кастомной коллекции");
+//            collectionSize = Integer.parseInt(in.next());
+//
+//            System.out.println("Для выбора заполнения коллекции случайными параметрами, введите \"random\"");
+//            System.out.println("Для выбора заполнения коллекции собственными значениями, введите \"self\"");
+//            System.out.println("Для выбора заполнения коллекции значениями из файла, введите \"file\"");
+//            System.out.println("Для выхода из программы введите команду \"exit\"");
+//            //String typeFillingCollection = in.next();
+//            if (in.next().equals("exit")) {
+//                in.close();
+//                workInMainCycle = false;
+//            }
+//            //List<Person> personCollection = fillCustomCollection(typeFillingCollection, collectionSize);
+//        }
+//        System.out.println("Выход из главного цикла");
 
 
-        // Привет, это Вадим, отпишитесь ниже - кто получил ссылку-приглашение.
-        // Привет, это Alexandro
+
     }
-
     public static CustomCollection<Person> fillCustomCollection(String arg, int collectionSize) {
         if (arg.equals("random")) {
 //            for (int i = 0; i < collectionSize; i++) {
@@ -39,5 +45,47 @@ public class Main {
 
         }
         return null;
+    }
+
+
+}
+
+final class PersonPull {
+    public static Person createPerson(PersonStrategy personStrategy) {
+        var b = new Person.Builder();
+        personStrategy.getPerson(b);
+        return b.build();
+    }
+}
+
+interface PersonStrategy {
+    void getPerson(Person.Builder b);
+}
+
+class RandomStrategy implements PersonStrategy {
+    private final Random rnd = new Random();
+    @Override
+    public void getPerson(Person.Builder b) {
+        b.name("User" + rnd.nextInt(100))
+                .age(18 + rnd.nextInt(50))
+                .weight(40 + rnd.nextInt(60));
+    }
+}
+
+class ManualStrategy implements PersonStrategy {
+    @Override
+    public void getPerson(Person.Builder b) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Укажите Имя");
+        String name = in.next();
+
+        System.out.println("Укажите возраст");
+        int age = Integer.parseInt(in.next());
+
+        System.out.println("Укажите вес");
+        double weight = Double.parseDouble(in.next());
+
+        b.name(name).age(age).weight(weight);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,22 +2,21 @@ public class Person {
     private final String name;
     private final int age;
     private final double weight;
-    private final int passNum;
 
     public Person(Builder builder) {
         this.name = builder.name;
         this.age = builder.age;
         this.weight = builder.weight;
-        this.passNum = builder.passNum;
     }
 
     public static class Builder {
-        private final String name;
+        private String name;
         private int age = 0;
         private double weight = 0.0;
-        private int passNum = 0;
-        public Builder(String name) {
+
+        public Builder name(String name) {
             this.name = name;
+            return this;
         }
         public Builder age(int age) {
             this.age = age;
@@ -27,18 +26,14 @@ public class Person {
             this.weight = weight;
             return this;
         }
-        public Builder passNum(int passNum) {
-            this.passNum = passNum;
-            return this;
-        }
-        public Person Build() {
+        public Person build() {
             return new Person(this);
         }
     }
 
     @Override
     public String toString() {
-        return "Person {Имя: " + name + " Вес: " + weight + " Возраст: " + age + " Номер паспорта: " + passNum + "}\n";
+        return "Person {Имя: " + name + "; Вес: " + weight + "; Возраст: " + age + "}\n";
     }
 }
 
