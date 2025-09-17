92c5191d14be27c06a4337d2f944088bb0fa725c
Alexandro1101 <sashkakuz02@mail.ru>
2025-09-17 16:19:08 +0300

    CollectionSorter.java

--- src/service/CollectionSorter.java
+++ src/service/CollectionSorter.java
@@ -2,72 +2,65 @@ package service;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CollectionSorter {
 
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(2);
+    
     public static <T> void sort(List<T> list, Comparator<T> comp) {
-        try (ForkJoinPool forkJoinPool = new ForkJoinPool(3))
-        {
-            forkJoinPool.invoke(new QuickSortTask<>(list, 0, list.size() - 1, comp));
+        try {
+            Future<?> future = threadPool.submit(() -> {
+                quickSort(list, 0, list.size() - 1, comp);
+            });
+            
+            future.get();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    private static class QuickSortTask<T> extends RecursiveAction {
-        private final List<T> list;
-        private final int low;
-        private final int high;
-        private final Comparator<T> comp;
-
-        QuickSortTask(List<T> list, int low, int high, Comparator<T> comp) {
-            this.list = list;
-            this.low = low;
-            this.high = high;
-            this.comp = comp;
-        }
-
-        @Override
-        protected void compute() {
-            if (low < high) {
-                // поиск индекса разделителя
-                int pivotIndex = partition(list, low, high, comp);
-                // рекурсивное деление коллекции элементов на левые части: от первого до разделителя - 1
-                QuickSortTask<T> left = new QuickSortTask<>(list, low, pivotIndex - 1, comp);
-                // рекурсивное деление коллекции элементов на правые части: от разделителя + 1 до последнего
-                QuickSortTask<T> right = new QuickSortTask<>(list, pivotIndex + 1, high, comp);
-                invokeAll(left, right);
-            }
+    
+    private static <T> void quickSort(List<T> list, int low, int high, Comparator<T> comp) {
+        if (low < high) {
+            int pivotIndex = partition(list, low, high, comp);
+            
+            quickSort(list, low, pivotIndex - 1, comp);
+            quickSort(list, pivotIndex + 1, high, comp);
         }
     }
-
-    // Поиск индекса разделителя
+    
     private static <T> int partition(List<T> list, int low, int high, Comparator<T> comp) {
-        T pivot = list.get(low); // получение опорного элемента - первый в передаваемом массиве
-        int left = low + 1;      // задание левого указателя - соседним с опорным элементом
-        int right = high;        // задание правого указателя - последний в передаваемом массиве
+        T pivot = list.get(low);
+        int left = low + 1;
+        int right = high;
 
-        while (left <= right) {             // пока указатели не пересекли друг-друга, или не указывают на 1 элемент
-            while (left <= right && comp.compare(list.get(left), pivot) <= 0)
-                left++;                     // поиск первого элемента >= опорного
-            while (left <= right && comp.compare(list.get(right), pivot) >= 0)
-                right--;                    // поиск первого элемента <= опорного
-            if (left < right) {             // если указатели не пересекли друг-друга
-                swap(list, left, right);    // перенос двух элементов на позиции указателей друг-друга
+        while (left <= right) {
+            while (left <= right && comp.compare(list.get(left), pivot) <= 0) {
+                left++;
+            }
+            while (left <= right && comp.compare(list.get(right), pivot) >= 0) {
+                right--;
+            }
+            if (left < right) {
+                swap(list, left, right);
                 left++;
                 right--;
             }
         }
-        swap(list, low, right); // перенос опорного элемента на место куда указывает указатель right
-        return right;           // возврат в качестве разделителя массива (int partition) - указателя right
+        swap(list, low, right);
+        return right;
     }
 
-    // Перестановка элементов
     private static <T> void swap(List<T> list, int i, int j) {
         T temp = list.get(i);
         list.set(i, list.get(j));
         list.set(j, temp);
+    }
+    
+    public static void shutdown() {
+        threadPool.shutdown();
     }
 }
