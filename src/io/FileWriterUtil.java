d8c0803f3f7088f49376a2790ab35adbb9c10739
Alexandro1101 <sashkakuz02@mail.ru>
2025-09-17 16:20:16 +0300

    FileWriterUtil.java

--- src/io/FileWriterUtil.java
+++ src/io/FileWriterUtil.java
@@ -1,17 +1,23 @@
 package io;
-import model.Person;
 
+import model.Person;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class FileWriterUtil {
 
-    public static void writePersonsToCSV(String filePath, List<Person> persons) throws IOException {
-        try (FileWriter writer = new FileWriter(filePath)) {
+    public static void writePersonsToCSV(String filePath, List<Person> persons, boolean append) throws IOException {
+        try (FileWriter writer = new FileWriter(filePath, append)) {
             for (Person p : persons) {
                 writer.write(p.getName() + "," + p.getAge() + "," + p.getWeight() + "\n");
             }
+        }
+    }
+
+    public static void writePersonToCSV(String filePath, Person person, boolean append) throws IOException {
+        try (FileWriter writer = new FileWriter(filePath, append)) {
+            writer.write(person.getName() + "," + person.getAge() + "," + person.getWeight() + "\n");
         }
     }
 }
