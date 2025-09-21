e5697135fac6a796056d51e04d7182d3d5f335e5
Ratio <rationabilitas@gmail.com>
2025-09-21 19:28:37 +0400

    19:28 21.09.25

--- src/factory/PersonPull.java
+++ src/factory/PersonPull.java
@@ -1,9 +1,7 @@
 package factory;
 
-import collection.CustomCollection;
 import model.Person;
 import strategy.PersonStrategy;
-import strategy.RandomStrategy;
 
 public final class PersonPull {
     public static Person createPerson(PersonStrategy personStrategy) {
